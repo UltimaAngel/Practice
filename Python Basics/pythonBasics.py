6c17175b9f5ecc76cbd30e31ec484783ac481ab4
Alex H. <thefpsnoob10@gmail.com>
2024-01-10 20:36:11 -0800

    pythonBasics made, variables implemented

---
+++ Python Basics/pythonBasics.py
@@ -0,0 +1,14 @@
+int5 = 5
+str5 = str(5)
+a, b, c = "Al", "Bo", "CJ"
+first = "the ONE"
+second = first + "?"
+funny = ["HEE", "HEY", "HAW"]
+l1, l2, l3 = funny
+#use a global variable somewhere
+#include more casting and data type examples
+
+print("Type of input: " + str(type(int5)))
+print(a, b, c)
+print(first, second)
+print(l1, l2, l3)
