85ca7325a1b22e705e3988a3e0a47f758fad806e
Alex H. <thefpsnoob10@gmail.com>
2024-01-21 13:04:01 -0800

    Added data type variables

--- Python Basics/pythonBasics.py
+++ Python Basics/pythonBasics.py
@@ -1,14 +1,38 @@
-int5 = 5
-str5 = str(5)
-a, b, c = "Al", "Bo", "CJ"
-first = "the ONE"
-second = first + "?"
-funny = ["HEE", "HEY", "HAW"]
-l1, l2, l3 = funny
-#use a global variable somewhere
-#include more casting and data type examples
+strTxt = "Hello world"
+intNum = 10
+floatNum = 10.5
+complexNum = 10j
+listSeq = ["a", "b", "c"]
+tupleSeq = ("d", "e", "f")
+rangeSeq = range(5)
+dictMap = {"a1" : 1, "b2" : 2}
+setSet = {"g", "h", "i"}
+frozeSet = frozenset({"j", "k", "l"})
+boolBool = False
+bytesBin = b"Test"
+bytArrBin = bytearray(4)
+memVewBin = memoryview(bytesBin)
+noType =  None
 
-print("Type of input: " + str(type(int5)))
+strCast = str(15)
+listCast = list(("A", "B", "C"))
+tupleCast = tuple(("D", "E", "F"))
+dictCast = dict(a3 = 3, b4 = 4)
+setCast = set(("G", "H", "I"))
+frozeCast = frozenset(("J", "K", "L"))
+boolCast = bool(2)
+bytesCast = bytes(4)
+
+a, b, c = "Aa", "Bb", "Cc"
+
+first = "part ONE"
+second = first + ", part TWO"
+
+lGroup = ["el", "al", "ol"]
+l1, l2, l3 = lGroup
+#use a global variable somewhere
+
+#print("Type of input: " + str(type(intNum)))
 print(a, b, c)
 print(first, second)
 print(l1, l2, l3)
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Practice
 A collection of practice projects used for my understanding of coding.
 
+# Python Basics
+Demonsrates how to use the variables in Python in the file's code.
+
 In Progress:
 - Python Basics
