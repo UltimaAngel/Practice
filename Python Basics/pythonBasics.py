729cdca7398b30829d17a565f625fc35cdfd3a5c
Alex H. <thefpsnoob10@gmail.com>
2024-01-27 17:03:13 -0800

    Added basic string examples

--- Python Basics/pythonBasics.py
+++ Python Basics/pythonBasics.py
@@ -30,7 +30,47 @@ floatStr = float("14.3")
 strInt = str(15)
 strFloat = str(16.4)
 
-#Play with strings next
+simpleStr = " Hey, sup"
+longStr = """This string is designed to be
+comically long. Note that it uses three
+double quotations on each end. Single
+quotes can be used as well."""
+numStr = "0123456789"
+formatStr = "Input values are: {}, {}, and {}"
+formatStrI = "Value 2: {2} | Value 1: {1} | Value 0: {0}"
+print("\tString START\n")
+
+print(simpleStr[3])
+for x in simpleStr:
+    print(x)
+print(len(simpleStr))
+if "string" in longStr:
+    print("Yep, string is in longStr.")
+if simpleStr not in longStr:
+    print("Nope, simpleStr is not in longStr.")
+
+print(numStr[2:6])
+print(numStr[:6])
+print(numStr[2:])
+print(numStr[-6:-3])
+
+print(simpleStr.upper())
+print(simpleStr.lower())
+print(simpleStr.strip())
+print(simpleStr.replace(",", "!"))
+print(simpleStr.split(", "))
+#There's many more string methods to show off.
+
+print(simpleStr + " - " + numStr)
+
+print(formatStr.format(21, 22, 23))
+print(formatStrI.format(24, 25, 26))
+
+print("A \'guy\' \\ dude\n made\troom\rfor \bodd \fstuff")
+
+
+print("\tString END\n")
+
 
 a, b, c = "Aa", "Bb", "Cc"
 
